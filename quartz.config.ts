2ab723d8ef8c9ae62e581b6c99b24d7e3e806438
benfreking123 <freking.benjamin@gmail.com>
2023-10-17 13:24:10 -0500

    base URL Update

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "benfreking123.github.io/IllumineProject",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
